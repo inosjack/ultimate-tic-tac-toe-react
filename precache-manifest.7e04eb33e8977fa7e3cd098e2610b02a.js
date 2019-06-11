0312186dd69da96f8a683ac0aab46bfc81fb8167
DESKTOP-C30S3TC\Jayant PC <inos.jay55@yahoo.com>
2019-06-11 10:20:03 +0530

    Updates

---
+++ precache-manifest.7e04eb33e8977fa7e3cd098e2610b02a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61179767934b958b2ae423a188e39d18",
+    "url": "/ultimate-tic-tac-toe-react/index.html"
+  },
+  {
+    "revision": "62ab4c3f073fd689ddf6",
+    "url": "/ultimate-tic-tac-toe-react/static/css/main.d09e2129.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/ultimate-tic-tac-toe-react/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "62ab4c3f073fd689ddf6",
+    "url": "/ultimate-tic-tac-toe-react/static/js/main.81579102.chunk.js"
+  },
+  {
+    "revision": "315d06198f16f120444e",
+    "url": "/ultimate-tic-tac-toe-react/static/js/runtime~main.40db7aa2.js"
+  }
+]);
